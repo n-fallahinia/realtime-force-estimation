35539dc45448a7f3461cfde08890b9cdea64a2ca
n-fallahinia <n.fallahinia@utah.edu>
2020-07-10 17:02:29 -0600

    load data final touch

--- utils/data_util.py
+++ utils/data_util.py
@@ -8,9 +8,27 @@ from __future__ import print_function
 
 import glob 
 import os  
+import sys
 import numpy as np
 from sklearn.model_selection import train_test_split
+from PIL import Image  
+import PIL 
 
+def lead_train_withRnd(subNum ,load_force = True, raw_images = False, train_ratio = 0.8):
+    """
+    load the training dataset from images in subject folders and store them 
+    in the train folder with random number of subIdx
+
+    Inputs:
+    - subNum: number of subjects to be sampled from
+    - load_force: Flag to wether load the coressponding labels or not
+    - raw_images: whther to load raw images or aligned images 
+    - train_ratio: ratio of train to test data 
+    """
+    subjIdx_list = np.random.choice(range(17), subNum, replace=False)
+    print('Subjects that are selected: %s' % subjIdx_list)
+    # lead_train_withIdx(subjIdx_list)
+    
 
 def lead_train_withIdx(subjIdx_list, load_force = True, raw_images = False, train_ratio = 0.8):
     """
@@ -57,6 +75,8 @@ def lead_train_withIdx(subjIdx_list, load_force = True, raw_images = False, trai
     print('Processing Done! ')
     print("Number of train data :{0:4d}, Number of test data :{1:4d}"
                 . format(len(data_to_write[0]), len(data_to_write[1]))) 
+    # data write part 
+    write_data(data_to_write, train_path, test_path)
 
 
 def load_force_txt(force_path, force_num, force_dim = 3):
@@ -90,11 +110,38 @@ def train_test_split_data(force_lists, image_lists, train_ratio ,validation = Tr
     return [force_lists_train, force_lists_test, image_lists_train, image_lists_test]
 
 def write_data(data_to_write, train_path, test_path):
-    ################################################################################
-    # TODO: should finish this function and also write a cross validation function #
-    ################################################################################
-    pass
+    """ write datat to a txt file """
+    force_lists_train = data_to_write[0]
+    force_lists_test = data_to_write[1]
+    image_lists_train = data_to_write[2]
+    image_lists_test = data_to_write[3]
+    # force data
+    with open(train_path+'/forces/force.txt', 'w') as filestream: 
+        for force in force_lists_train:
+            filestream.write("%s,%s,%s\n"%(force[0], force[1], force[2]))
+    print('Train forces saved! ')
 
-if __name__ == "__main__":
-    subjIdx_list = [1,3,5]
-    lead_train_withIdx(subjIdx_list)
+    with open(test_path+'/forces/force.txt', 'w') as filestream: 
+        for force in force_lists_test:
+            filestream.write("%s,%s,%s\n"%(force[0], force[1], force[2]))
+    print('Test forces saved! ')
+    # image data
+    for Idx, train_image in enumerate(image_lists_train):
+        img = Image.open(train_image)
+        img.save(train_path+'/image/img_'+f'{Idx:04d}.jpg')
+        if Idx%100 == 0:
+            print('\t%d images are saved'% Idx);       
+    print('Train images saved! ')
+
+    for Idx, test_image in enumerate(image_lists_test):
+        img = Image.open(test_image)
+        img.save(test_path+'/image/img_'+f'{Idx:04d}.jpg')
+        if Idx%100 == 0:
+            print('\t%d images are saved'% Idx);  
+    print('Test images saved! ')
+
+# if __debug__:
+#     print('debug')
+#     subjIdx_list = [1,3,5]
+#     lead_train_withRnd(5)
+#     lead_train_withIdx(subjIdx_list)
