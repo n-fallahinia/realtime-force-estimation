59782810b26451ad4f357175afec27e80fc514b3
n-fallahinia <n.fallahinia@utah.edu>
2020-08-31 17:07:46 -0600

    new OONX update

---
+++ convert2oonx.py
@@ -0,0 +1,79 @@
+"""
+Script to convert the NailNet model from tf and Keras to oonx 
+which can be used for both visualization with netron and inference
+with nvidia TensorRT (speeds up the inference by 60%)
+Navid Fallahinia - 07/11/2020
+BioRobotics Lab
+"""
+
+import argparse
+import os
+import sys
+from subprocess import check_call
+from packaging import version
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
+
+import tensorflow as tf
+from tensorflow import keras
+
+import tf2onnx
+# import keras2onnx
+
+PYTHON = sys.executable
+parser = argparse.ArgumentParser()
+
+parser.add_argument('--model_dir', default='./log/20200827-170907',
+                    help="log directory containing the saved model")
+
+parser.add_argument('--mode', default='tf', 
+                    help="convert from keras or tensorflow model")
+
+parser.add_argument('--output_dir', default='model.onnx', 
+                    help="output model name")
+
+parser.add_argument('--v', default=True,
+                    help ='visualization with Netron')
+
+if __name__ == '__main__':
+
+    print("TensorFlow version: ", tf.__version__)
+    assert version.parse(tf.__version__).release[0] >= 2, \
+    "This notebook requires TensorFlow 2.0 or above."
+
+    # Load the model file
+    args = parser.parse_args()
+
+    # check for the required mode
+    if args.mode == 'tf':
+        model_dir = os.path.join(args.model_dir, 'best_full_model_path')
+        assert os.path.exists(model_dir), "No tf model found at {}".format(model_dir) 
+        output_dir = os.path.join(args.model_dir, args.output_dir)
+
+        # Launch training with this config
+        cmd = "{python} -m tf2onnx.convert --saved-model {model_dir} --output {output_dir}".format(python=PYTHON,
+            model_dir=model_dir, output_dir=output_dir)
+        print(cmd)
+        check_call(cmd, shell=True)
+        print('Done!')
+
+    elif args.mode == 'keras':
+        model_path = args.model_dir
+        assert os.path.isfile(model_path), "No tf model found at {}".format(model_path)   
+        # load model
+# +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+        # TODO
+        # model = tf.keras.models.load_model(model_path)
+        # onnx_model = keras2onnx.convert_keras(model, model.name)
+# +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+    else:
+        print('invalid mode')
+
+    if args.v:
+        # Launch netron with this config
+        cmd = "netron {output_dir}".format(output_dir=output_dir)
+        print(cmd)
+        check_call(cmd, shell=True)
+
+
+
+    
--- model/input_fn.py
+++ model/input_fn.py
@@ -2,22 +2,24 @@
 import tensorflow as tf
 from model.utils.data_util import *
 
-def preprocess_data(image, force, size, use_random_flip, use_random_color):
+def preprocess_data(image, force, size, use_random_flip, use_random_color, use_hsv):
     """ preprocessing images """   
 
     image_string = tf.io.read_file(image)
-    image = load_image(image_string, size)
+    image = load_image(image_string, size, use_hsv)
     image = augment_image(image, use_random_flip, use_random_color)
 
     return image, force
 
 
-def load_image(image_string, size):
+def load_image(image_string, size, use_hsv):
     """ decoding the images """   
 
     image = tf.image.decode_jpeg(image_string, channels=3)
     image = tf.image.convert_image_dtype(image, tf.float32) 
     image = tf.image.resize(image, [size, size]) 
+    if use_hsv:
+        image = tf.image.rgb_to_hsv(image)
 
     return image
 
@@ -32,6 +34,7 @@ def augment_image(image, use_random_flip, use_random_color):
         image = tf.image.random_brightness(image, max_delta=32.0 / 255.0)
         image = tf.image.random_contrast(image, lower=0.5, upper=1.5)
 
+    # image = tf.image.adjust_saturation(image,0.5)
     image = tf.clip_by_value(image, 0.0, 1.0)
 
     return image
@@ -49,8 +52,8 @@ def input_fn(is_training, filenames, labels, params = None):
     assert len(filenames) == len(labels), "Filenames and labels should have same length"
 
     # Create a Dataset serving batches of images and labels
-    preproc_train_fn = lambda f, l: preprocess_data(f, l, params.image_size, params.use_random_flip, params.use_random_color)
-    preproc_eval_fn = lambda f, l: preprocess_data(f, l, params.image_size, False, False)
+    preproc_train_fn = lambda f, l: preprocess_data(f, l, params.image_size, params.use_random_flip, params.use_random_color, params.use_hsv)
+    preproc_eval_fn = lambda f, l: preprocess_data(f, l, params.image_size, False, False, params.use_hsv)
 
     if is_training:
         dataset = (tf.data.Dataset.from_tensor_slices((filenames, labels))
